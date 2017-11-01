fc90131b625e7ceebbda65f7c0d61ae2112a99df
Rhett Rogers <serenade.xs@icloud.com>
2017-11-01 14:20:53 -0600

    Creates modules

---
+++ ObserverSet iOS/ObserverSet.h
@@ -0,0 +1,19 @@
+//
+//  ObserverSet.h
+//  ObserverSet
+//
+//  Created by Rhett Rogers on 11/1/17.
+//  Copyright © 2017 LDS Church. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObserverSet.
+FOUNDATION_EXPORT double ObserverSetVersionNumber;
+
+//! Project version string for ObserverSet.
+FOUNDATION_EXPORT const unsigned char ObserverSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObserverSet/PublicHeader.h>
+
+
---
+++ ObserverSet macOS/ObserverSet_macOS.h
@@ -0,0 +1,19 @@
+//
+//  ObserverSet_macOS.h
+//  ObserverSet macOS
+//
+//  Created by Rhett Rogers on 11/1/17.
+//  Copyright © 2017 LDS Church. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ObserverSet_macOS.
+FOUNDATION_EXPORT double ObserverSet_macOSVersionNumber;
+
+//! Project version string for ObserverSet_macOS.
+FOUNDATION_EXPORT const unsigned char ObserverSet_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObserverSet_macOS/PublicHeader.h>
+
+
